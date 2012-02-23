1ea6895b310e0cf7a8713b04048b59ed0a2c4514
Gabriel Somoza <work@gabrielsomoza.com>
2012-02-23 12:10:19 +0100

    Relicensed to GPLv2

--- Kuler/Api.php
+++ Kuler/Api.php
@@ -10,12 +10,12 @@
  * @package PHP Kuler API Library
  * @link http://learn.adobe.com/wiki/display/kulerdev/B.+Feeds
  * 
- * @copyright Copyright (c) 2011, Gabriel Somoza
+ * @copyright Copyright (c) 2011-2012, Gabriel Somoza
  * 
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
  * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -23,7 +23,8 @@
  * GNU General Public License for more details.
  * 
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this program; if not, see <http://www.gnu.org/licenses/>.
+ * 
  */
 
 define('KULER_PATH', realpath(dirname(__FILE__)));
